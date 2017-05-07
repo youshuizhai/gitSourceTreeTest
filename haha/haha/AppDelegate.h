ecb9e584959a8c53fe6662f59c31956bbf776e2a
TOT <843450560@qq.com>
2017-05-07 15:37:29 +0800

    定义了一个没有mytest方法

---
+++ haha/haha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  haha
+//
+//  Created by TOT on 2017/5/7.
+//  Copyright © 2017年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

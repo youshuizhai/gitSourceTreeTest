470292720267cdb8964ebc2f49bafe896ea5ab13
TOT <843450560@qq.com>
2017-05-07 16:01:15 +0800

    caom\nimama

--- haha/haha/ViewController.m
+++ haha/haha/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"caonimei")
+    NSLog(@"caonimei");
+     NSLog(@"caonima");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
